3162694328732f29ec3537b05f9f0a3f1be57f05
Vontell <sankarsh.rajeev@gmail.com>
2023-07-20 17:40:53 -0700

    Hero Banner Added

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Box, Stack, Typography, Button, TextField } from '@mui/material';
+
+const SearchExercises = () => {
+    return (
+        <Box>
+
+        </Box>
+    );
+}
+
+export default SearchExercises;
