72d2fa29a1c63c0808943930413cb016cb5426e4
Vontell <sankarsh.rajeev@gmail.com>
2023-07-24 16:33:28 -0700

    Files added

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -6,20 +6,21 @@ import Icon from '../assets/icons/gym.png';
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
     return (
         <Stack type='button' alignItems='center' justifyContent='center' className='bodyPart-card'
-         sx={{
-                borderTop:  bodyPart === item ? '4px solid #ff2625':'',
+            sx={{
+                borderTop: bodyPart === item ? '4px solid #ff2625' : '',
                 backgroundColor: '#fff',
                 borderBottomLeftRadius: '20px',
                 width: '270px',
                 height: '280px',
                 cursor: 'pointer',
                 gap: '47px'
-         }} 
+            }}
 
-        
-        onCLick={() =>{
-            setBodyPart(item);
-        }}
+
+            onClick={() => {
+                setBodyPart(item);
+                window.scrollTo({top:1800,left:100, behavior:'smooth'})
+            }}
         >
             <img src={Icon} alt='Dumbbell' style={{ width: '40px', height: "40px" }} />
             <Typography fontSize="25px" fontWeight="bold" color="#3a1212" textTransform="capitalize" >{item}</Typography>
@@ -29,4 +30,3 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
 }
 
 export default BodyPart;
- 
